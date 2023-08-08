09b42abd29d1f4082d0d67d074a634477aaa75f2
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-09 00:41:15 +0200

    Validate input price and discount

--- descuento.js
+++ descuento.js
@@ -1,11 +1,37 @@
-const price = document.querySelector(".price")
-const discount = document.querySelector(".discount")
+const inputPrice = document.querySelector("#price")
+const inputDiscount = document.querySelector("#discount")
 const paragraph = document.querySelector(".paragraph")
 const calculateBtn = document.querySelector(".calculateBtn")
 
 calculateBtn.addEventListener("click", calculate)
+inputDiscount.addEventListener("onBlur", validateDiscount)
 
 function calculate() {
-  const result = (price.value * (100 - discount.value)) / 100
-  paragraph.innerText = "Precio con descuento:: " + result
+  if (
+    validateDiscount(inputDiscount.value) &&
+    inputHasAValue(inputPrice.value, inputDiscount.value)
+  ) {
+    const price = inputPrice.value
+    const discount = inputDiscount.value
+    const result = (price * (100 - discount)) / 100
+    paragraph.innerText = "Precio con descuento: " + result
+  }
 }
+
+function validateDiscount(value) {
+  if (value < 0 || value > 100) {
+    paragraph.innerText = "El descuento debe ser entre 0% y 100%"
+    return false
+  }
+  return true
+}
+
+function inputHasAValue(price, discount) {
+  if (!price || !discount) {
+    paragraph.innerText = "Debes rellenar el precio y el descuento"
+    return false
+  }
+  return true
+}
+
+// crear cupones de descuento
