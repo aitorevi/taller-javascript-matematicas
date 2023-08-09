aa236a597754ada29e3fe8f2e5a22507b978ba41
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-09 12:52:50 +0200

    Version 1.0 coupons

--- descuento.js
+++ descuento.js
@@ -1,10 +1,13 @@
 const inputPrice = document.querySelector("#price")
 const inputDiscount = document.querySelector("#discount")
+const inputCoupon = document.querySelector("#coupon")
 const paragraph = document.querySelector(".paragraph")
 const calculateBtn = document.querySelector(".calculateBtn")
+const couponBtn = document.querySelector(".couponBtn")
 
 calculateBtn.addEventListener("click", calculateTheDiscountedPrice)
 inputDiscount.addEventListener("onBlur", discountIsWithinTheRange)
+couponBtn.addEventListener("click", extractDiscountCoupon)
 
 const MIN_PERCENTAGE_DISCOUNT = 0
 const MAX_PERCENTAGE_DISCOUNT = 100
@@ -37,4 +40,32 @@ function inputHasAValue(price, discount) {
   return true
 }
 
-// crear cupones de descuento
+const discountCouponsList = {
+  VERANO20: 20,
+  PLATZI15: 15,
+  OFERTA10: 10,
+}
+
+function extractDiscountCoupon() {
+  const couponKey = inputCoupon.value
+  for (key in discountCouponsList) {
+    if (key === couponKey) {
+      inputDiscount.setAttribute("value", discountCouponsList[key])
+      return
+    }
+  }
+  paragraph.innerText = "Ese cupón no existe"
+}
+
+// Implementación de los cupones aplicando el descuento directamente al precio y mostrando el resultado
+
+const inputPrice2 = document.querySelector("#price2")
+const inputCoupon2 = document.querySelector("#coupon2")
+const paragraph2 = document.querySelector(".paragraph2")
+const couponBtn2 = document.querySelector(".couponBtn2")
+
+// comprobar si existe o no el cupon
+
+// si no existe mostrar el mensaje de error
+
+// si existe, aplicar el descuento y mostrar el precio con descuento
