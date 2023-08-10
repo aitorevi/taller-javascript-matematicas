19ee6bb0aeec8b330c4834990596181fba788b4a
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-10 16:39:47 +0200

    Calculation median: fixed bug and added floor() method that does the same as the trunc() method

--- median.js
+++ median.js
@@ -1,14 +1,15 @@
-const valuesList = [20, 30, 10, 30]
+const valuesList = [30, 10, 40, 25]
 
 const sortedValuesList = valuesList.sort((a, b) => a - b)
 
 if (isOdd(valuesList)) {
-  const median = valuesList[Math.trunc(valuesList.length / 2)]
+  // const median = valuesList[Math.trunc(valuesList.length / 2)]
+  const median = valuesList[Math.floor(valuesList.length / 2)]
   console.log("La mediana es: " + median)
 } else {
   const firstCoreValue = valuesList[valuesList.length / 2 - 1]
   const secondCoreValue = valuesList[valuesList.length / 2]
-  const median = firstCoreValue + secondCoreValue
+  const median = (firstCoreValue + secondCoreValue) / 2
   console.log("La mediana es: " + median)
 }
 
