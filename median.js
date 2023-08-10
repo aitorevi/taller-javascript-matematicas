5b350c3bcfb90f017a727f25b0377a70188112d3
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-10 19:31:02 +0200

    Add function calculateMedian() and returns + console.log() of median

--- median.js
+++ median.js
@@ -1,21 +1,24 @@
-const valuesList = [30, 10, 40, 25, 8]
+const valuesList = [30, 10, 25, 8]
 
-const sortedValuesList = valuesList.sort((a, b) => a - b)
+console.log("La mediana es: " + calculateMedian(valuesList))
 
-if (isOdd(valuesList)) {
-  // const median = valuesList[Math.trunc(valuesList.length / 2)]
-  const median = valuesList[Math.floor(valuesList.length / 2)]
-  console.log("La mediana es: " + median)
-} else {
-  const firstCoreValue = valuesList[valuesList.length / 2 - 1]
-  const secondCoreValue = valuesList[valuesList.length / 2]
-  const coreValues = [firstCoreValue, secondCoreValue]
-  const median = coreValues.reduce((a, b) => a + b) / 2
-  console.log("La mediana es: " + median)
+function calculateMedian(valuesList) {
+  const sortedValuesList = valuesList.sort((a, b) => a - b)
+  if (isOdd(sortedValuesList)) {
+    // const median = sortedValuesList[Math.trunc(valuesList.length / 2)]
+    const median = sortedValuesList[Math.floor(sortedValuesList.length / 2)]
+    return median
+  } else {
+    const firstCoreValue = sortedValuesList[sortedValuesList.length / 2 - 1]
+    const secondCoreValue = sortedValuesList[sortedValuesList.length / 2]
+    const coreValues = [firstCoreValue, secondCoreValue]
+    const median = coreValues.reduce((a, b) => a + b) / 2
+    return median
+  }
 }
 
-function isOdd(valuesList) {
-  const quantityOfElements = valuesList.length
+function isOdd(sortedValuesList) {
+  const quantityOfElements = sortedValuesList.length
   if (quantityOfElements % 2 == 0) {
     return false
   }
