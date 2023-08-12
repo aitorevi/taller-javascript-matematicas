f77044c2b309cb57faebfc40d5f550396ded06a5
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-08-13 00:11:22 +0200

    Refactor, two for loops suppressed

--- fashion.js
+++ fashion.js
@@ -1,17 +1,8 @@
 // Moda: El elemento que mas veces se repite en un array, si hay varios que se repiten las mismas veces se llama bimodal.
 
-// Primero la solución sencilla, después implementaré la solución en la que dos números se repitan las mismas veces.ç
-
-//  ¿Qué necesito?
-
-// un array con valores repetidos
 const valuesList = [30, 30, 30, 20, 20, 12, 1, 12, 5, 5, 5, 5, 1, 1, 1, 12, 12]
 
-// recorrer el array, comprobar si el valor existe en un array de objetos, este array de objetos, tiene objetos con el valor y el numero de veces que ha salido ese valor.
-
 const repetitionsOfValue = []
-
-// tendria que ser una funcion que reciba el valuesList, el repetitionsOfValue
 
 action(valuesList, repetitionsOfValue)
 
@@ -33,19 +24,36 @@ function action(valuesList, repetitionsOfValue) {
   }
   console.log(repetitionsOfValue)
 
-  let maxValueRepetition = 0
-  for (element of repetitionsOfValue) {
-    if (element.repeticiones > maxValueRepetition) {
-      maxValueRepetition = element.repeticiones
-    }
-  }
+  // Hacer un HackMd con esto de abajo, super interesante la solución y muy elegante. Acceder a un array de objetos combinando Math.max con map.
 
-  let elementsRepit = []
-  for (element of repetitionsOfValue) {
-    if (element.repeticiones == maxValueRepetition) {
-      elementsRepit.push(element)
-    }
-  }
+  // Este bucle sustituirlo por Math.Max
+  //   let maxValueRepetition = 0
+  //   for (element of repetitionsOfValue) {
+  //     if (element.repeticiones > maxValueRepetition) {
+  //       maxValueRepetition = element.repeticiones
+  //     }
+  //   }
+
+  const maxValueRepetition = Math.max(
+    ...repetitionsOfValue.map((element) => element.repeticiones)
+  )
+
+  // Hacer un HackMd con esto de abajo
+
+  // Este bucle for sustituirlo por filter
+  //   let elementsRepit = []
+  //   for (element of repetitionsOfValue) {
+  //     if (element.repeticiones == maxValueRepetition) {
+  //       elementsRepit.push(element)
+  //     }
+  //   }
+
+  const elementsRepit = repetitionsOfValue.filter(
+    (element) => element.repeticiones == maxValueRepetition
+  )
+
+  console.log("Linea 64")
+  console.log(elementsRepit)
 
   elementsRepit.length == 1
     ? console.log("La moda es: " + elementsRepit[0].valor)
